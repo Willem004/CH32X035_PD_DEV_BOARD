8665425dae2387b3b44622f0de85e644a8a9c0f8
Willem004 <135047796+Willem004@users.noreply.github.com>
2024-08-04 23:57:02 +0200

    Expanded the test code a bit to include some basic debuging via uart 1

--- CODE/CH32X035-TEST/TEST/src/main.c
+++ CODE/CH32X035-TEST/TEST/src/main.c
@@ -1,8 +1,14 @@
 
 
 #include "ch32x035.h"
+#include "debug.h"
+#include "ch32x035_usbpd.h"
 
 int main(void)
 {
-    // NOP();
+    USART_Printf_Init(9600);
+    while (1)
+    {
+        printf("HelloWorld\n");
+    }
 }
